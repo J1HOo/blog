4dda6254c419465bc01bd15c0de9ba0cfb7ac440
박지호 <businessj1ho@gmail.com>
2021-07-28 16:15:44 +0900

    add for ArticleAddPage

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { articles } from "../dummy";
 
 export const ArticlePage = () => {
-  return <>ArticlePage</>;
+  const { goBack } = useHistory();
+  const { id } = useParams<{ id: string }>();
+  const article = articles.find((article) => article.id === Number(id));
+
+  if (!article) return <>404, Not Found</>;
+  return (
+    <>
+      <button onClick={goBack}>뒤로가기</button>
+      <h1>{article.title}</h1>
+      <img src={article.thumbnail} alt="thumbnail" />
+      <p className="text-gray-500 text-sm">{article.content}</p>
+    </>
+  );
 };
